f1b9969a1119468a80b3b29d188d57de9b88df37
Ryoichi Suganuma <ryoichi.suganuma@satori.team>
2021-09-27 02:40:13 +0900

    my own modifications

--- lib/spf_lookup/dns_lookup_counter.rb
+++ lib/spf_lookup/dns_lookup_counter.rb
@@ -1,5 +1,6 @@
 require 'coppertone'
 require_relative './spf_record_fetcher'
+require_relative './error'
 
 module SpfLookup
   class DNSLookupCounter
@@ -7,9 +8,17 @@ module SpfLookup
     LOOKUP_LIMIT_SPECIFIED_BY_RFC7208 = 10
 
     class << self
-
       def count_dns_lookup(domain)
-        return __count__(domain, lookup_count = 0, depth = 1)
+        spf_record = find_spf_record(domain)
+
+        domains = fetch_lookup_target_domains(spf_record)
+
+        _lookup_count = spf_record.dns_lookup_term_count
+        domains.compact.each do |domain|
+          _lookup_count += count_dns_lookup(domain)
+        end
+
+        return _lookup_count
       end
 
       def count_is_valid?(domain)
@@ -17,32 +26,37 @@ module SpfLookup
       end
 
       private
-      def __count__(domain, lookup_count = 0, depth = 0)
-        counter      = -> (domain) { __count__(domain, lookup_count, depth+1) }
-        text_fetcher = -> (obj)    { obj.domain_spec.macro_text }
 
-        record_fetcher.txt_record_values(domain).each do |value|
-          next unless Coppertone::Record.record?(value)
+      def fetch_lookup_target_domains(spf_record)
+        domain_fetcher = -> (obj) { obj.domain_spec.macro_text }
 
-          record = Coppertone::Record.new(value)
-          lookup_count += record.dns_lookup_term_count
+        domains = spf_record.includes.each_with_object([]) { |include_value, memo|
+          memo << domain_fetcher.(include_value.mechanism)
+        }
+        domains << domain_fetcher.(spf_record.redirect) unless spf_record.redirect.nil?
 
-          return lookup_count if lookup_count > LOOKUP_LIMIT_SPECIFIED_BY_RFC7208
+        return domains
+      end
 
-          record.includes.each { |include_value|
-            lookup_count = counter.call(text_fetcher.(include_value.mechanism))
-          }
+      def find_spf_record(domain)
+        spf_record = record_fetcher.txt_record_values(domain).each_with_object([]) {|txt_record_value, memo|
+          memo << Coppertone::Record.new(txt_record_value) if Coppertone::Record.record?(txt_record_value)
+        }
 
-          lookup_count = counter.call(text_fetcher.(record.redirect)) unless record.redirect.nil?
+        case spf_record.length
+        when 1 then
+          return spf_record.first
+        when 0 then
+          raise SpfLookup::SpfRecordNotFound.new("#{domain} does not have an spf record.")
+        else
+          raise SpfLookup::MultipleSpfRecordError.new("There must be only one SPF record in the same domain.")
         end
-
-        return lookup_count
       end
 
       def record_fetcher
         @fetcher ||= SPFRecordFetcher.new(SpfLookup::DNS_CONFIG[:option])
       end
-
     end
   end
+
 end
